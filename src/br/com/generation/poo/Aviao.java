68ec1938837f16e78bd9e9664d5dc106a3536398
josiasveras <josias_veras@hotmail.com>
2021-12-15 14:06:24 -0300

    Exercício 02 de POO em Java

---
+++ src/br/com/generation/poo/Aviao.java
@@ -0,0 +1,22 @@
+package br.com.generation.poo;
+
+/*
+ * 2) Crie uma classe avião e apresente os atributos e métodos referentes
+	  esta classe, em seguida crie um objeto avião, defina as instancias deste
+      objeto e apresente as informações deste objeto no console. 
+*/
+
+public class Aviao {
+
+	String modelo;
+	int capacidade;
+	String cor;
+	
+	void voar() {
+		System.out.println("O avião está voando...");
+	}
+	
+	void pousar() {
+		System.out.println("O avião está pousando...");
+	}
+}
--- src/br/com/generation/poo/TestaClasse.java
+++ src/br/com/generation/poo/TestaClasse.java
@@ -7,12 +7,49 @@ public class TestaClasse {
 		//Testa Cliente
 		Cliente c1 = new Cliente("Paulo Santos", 30, "25-12-1991", "masculino");
 		Cliente c2 = new Cliente("Janaina Oliveira", 25, "08-06-1995", "feminino");
+		
+		System.out.println("--------------------------------------------------------TESTA CLIENTE--------------------------------------------------------");
 		System.out.println(c1);
 		System.out.println(c2);
 		System.out.println("O nome do cliente é: " + c1.getNome());
 		System.out.println("O nome da cliente é: " + c2.getNome());
 		
+		System.out.println();
 		
+		//Testa Aviao
+		Aviao av1 = new Aviao();
+		Aviao av2 = new Aviao();
+		Aviao av3 = new Aviao();
+		
+		System.out.println("--------------------------------------------------------TESTA AVIAO--------------------------------------------------------");
+		
+		av1.modelo = "A350 XWB";
+		av1.capacidade = 270;
+		av1.cor = "branca";
+		
+		av2.modelo = "Airbus A320";
+		av2.capacidade = 132;
+		av2.cor = "cinza";
+		
+		av3.modelo = "Boeing 787";
+		av3.capacidade = 242;
+		av3.cor = "azul e branca";
+		
+		System.out.println("O modelo é: " + av1.modelo + ". A capacidade de passageiros é: " + av1.capacidade + ". A cor é: " + av1.cor);
+		av1.voar();
+		av1.pousar();
+		
+		System.out.println();
+		
+		System.out.println("O modelo é: " + av2.modelo + ". A capacidade de passageiros é: " + av2.capacidade + ". A cor é: " + av2.cor);
+		av1.voar();
+		av1.pousar();
+
+		System.out.println();
+		
+		System.out.println("O modelo é: " + av3.modelo + ". A capacidade de passageiros é: " + av3.capacidade + ". A cor é: " + av3.cor);
+		av1.voar();
+		av1.pousar();
 	}
 
 }
